63fdcbf3ca73302da7547717f133f09933571e3b
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-02-20 22:22:55 -0500

    refactor: add the git command to install the NVIM DAP plugin

--- init.sh
+++ init.sh
@@ -6,3 +6,6 @@ brew install neovim tmux ripgrep git
 # Install the Packer package manager
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# Install NVIM DAP
+git clone https://github.com/mfussenegger/nvim-dap.git ~/.config/nvim/pack/plugins/start/nvim-dap
