07d63f734a74fdb4f7258c750863f8956caef663
Jean-Frederic Mainville <jfm@pixmob.com>
2024-02-28 11:56:07 -0500

    refactor: sanitize the remap configurations

--- lua/jfmainville/remap.lua
+++ lua/jfmainville/remap.lua
@@ -1,50 +1,28 @@
-
+-- Define the leader action to be the space bar
 vim.g.mapleader = " "
+-- Navigate back to the file explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
+-- Move the currently selected line down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- Move the currently selected line up
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
+-- Bring the line below up
 vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- Go up mulitple lines
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end)
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end)
-
+-- Go down multiple lines
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
--- This is going to get me cancelled
+-- Return to normal mode
 vim.keymap.set("i", "<C-c>", "<Esc>")
-
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+-- format the current file
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
+-- Replace current word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- Add the execute permission to the current file
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
+-- Source the current file
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
