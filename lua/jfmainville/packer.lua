d1cc1aed53f22aa02e797cd0fd9e36e53f2ddc5e
Jean-Frederic Mainville <jfm@pixmob.com>
2024-03-01 13:24:27 -0500

    feat: add the nvim-web-deviconds plugin to Packer

--- lua/jfmainville/packer.lua
+++ lua/jfmainville/packer.lua
@@ -16,6 +16,7 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use("mbbill/undotree")
   use("tpope/vim-fugitive")
+  use("nvim-tree/nvim-web-devicons")
 
   use({
     "stevearc/oil.nvim",
