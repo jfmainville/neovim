1f7713dfbad2e9b0cb2297560679a3475e7d7daa
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-02-29 22:14:16 -0500

    feat: add the vim-vinegar plugin to the project

--- lua/jfmainville/packer.lua
+++ lua/jfmainville/packer.lua
@@ -16,6 +16,7 @@ return require('packer').startup(function(use)
   use('theprimeagen/harpoon')
   use("mbbill/undotree")
   use("tpope/vim-fugitive")
+  use("tpope/vim-vinegar")
 
   use({
     "stevearc/conform.nvim",
