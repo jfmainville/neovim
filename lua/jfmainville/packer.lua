6b1364dd7e8f2d06f83652c0d05f36b1b2ee21fc
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-03-02 14:01:43 -0500

    refactor: format the packer.lua file

--- lua/jfmainville/packer.lua
+++ lua/jfmainville/packer.lua
@@ -1,26 +1,31 @@
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+  use "wbthomason/packer.nvim"
 
   -- Themes
   use({ "navarasu/onedark.nvim", as = "onedark" })
-  use({
-    'folke/tokyonight.nvim',
-    as = 'tokyonight',
-    config = function()
-      vim.cmd('colorscheme tokyonight-night')
-    end
-  })
 
-  use({ 'nvim-telescope/telescope.nvim', tag = '0.1.5', requires = { { 'nvim-lua/plenary.nvim' } } })
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-  use('theprimeagen/harpoon')
+  -- Plugins
+  use({
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.5",
+    requires = {
+      { "nvim-lua/plenary.nvim" },
+    }
+  })
+  use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+  use("theprimeagen/harpoon")
   use("mbbill/undotree")
   use("tpope/vim-fugitive")
   use("nvim-tree/nvim-web-devicons")
-  use({ "ggandor/leap.nvim", requires = { "tpope/vim-repeat" } })
-  use({ "nvim-lualine/lualine.nvim", requires = { 'nvim-tree/nvim-web-devicons', opt = true } })
+  use({
+    "ggandor/leap.nvim",
+    requires = {
+      { "tpope/vim-repeat" }
+    }
+  })
+  use({ "nvim-lualine/lualine.nvim", requires = { "nvim-tree/nvim-web-devicons", opt = true } })
   use("stevearc/oil.nvim")
   use("stevearc/conform.nvim")
   use({
@@ -33,25 +38,25 @@ return require('packer').startup(function(use)
     }
   })
   use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
     requires = {
       -- LSP Support
-      { 'neovim/nvim-lspconfig' },
-      { 'williamboman/mason.nvim' },
-      { 'williamboman/mason-lspconfig.nvim' },
+      { "neovim/nvim-lspconfig" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
 
       -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-nvim-lua' },
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-nvim-lua" },
 
       -- Snippets
-      { 'L3MON4D3/LuaSnip' },
-      { 'rafamadriz/friendly-snippets' },
+      { "L3MON4D3/LuaSnip" },
+      { "rafamadriz/friendly-snippets" },
     }
   }
 end)
