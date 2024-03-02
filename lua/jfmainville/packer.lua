ea1801e4aed4e1d335aa66099d9ab907f2186cee
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-03-02 14:01:15 -0500

    refactor: add the OneDark color scheme

--- lua/jfmainville/packer.lua
+++ lua/jfmainville/packer.lua
@@ -3,6 +3,8 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
+  -- Themes
+  use({ "navarasu/onedark.nvim", as = "onedark" })
   use({
     'folke/tokyonight.nvim',
     as = 'tokyonight',
