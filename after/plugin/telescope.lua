2d4d0f88ae20272109124266e2bfd2d29da57bb1
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-03-03 13:20:59 -0500

    refactor: update the telescope plugin base configuration

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,11 +2,11 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
-require("telescope").setup({
+require('telescope').setup({
   defaults = {
     -- Remove the ripgrep indentation
     vimgrep_arguments = {
@@ -22,7 +22,7 @@ require("telescope").setup({
   },
   pickers = {
     find_files = {
-      hidden = true
+      hidden = false
     }
   }
-}
+})
