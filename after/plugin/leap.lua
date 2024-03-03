3e4b52e38ad83c812a6ab56325b70c4365f9f7f0
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-03-03 13:20:13 -0500

    refactor: add the safe_labels parameter to the leap plugin

--- after/plugin/leap.lua
+++ after/plugin/leap.lua
@@ -2,3 +2,4 @@ require('leap').create_default_mappings()
 require('leap').opts.special_keys.prev_target = '<bs>'
 require('leap').opts.special_keys.prev_group = '<bs>'
 require('leap.user').set_repeat_keys('<cr>', '<bs>')
+require('leap').opts.safe_labels = {}
