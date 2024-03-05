4677c5d25cb39cb60c45220aa3121857e03915ff
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-03-04 21:20:30 -0500

    refactor: add a keymap to open the ChatGPT window

--- lua/jfmainville/remap.lua
+++ lua/jfmainville/remap.lua
@@ -37,3 +37,5 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
   vim.cmd("so")
 end)
+-- Open ChatGPT window
+vim.keymap.set("n", "<leader>cg", "<cmd>ChatGPT<CR>")
