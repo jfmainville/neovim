8a5211992fdaab441bccee6d3dfaaa2c6b4a2605
Jean-Frederic Mainville <jfm@pixmob.com>
2024-03-07 16:09:08 -0500

    refactor: add the copy command for the lazygit configuration file

--- init.sh
+++ init.sh
@@ -9,3 +9,5 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 
 # Install NVIM DAP
 git clone https://github.com/mfussenegger/nvim-dap.git ~/.config/nvim/pack/plugins/start/nvim-dap
+
+cp ./lazygit/config.yaml ~/Library/Application\ Support/lazygit/
