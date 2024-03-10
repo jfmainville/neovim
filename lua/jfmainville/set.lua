fb04f6f5183e46b724f7df98c1181ae700a3824c
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-03-09 21:11:51 -0500

    refactor: disable the default spell check functionality

--- lua/jfmainville/set.lua
+++ lua/jfmainville/set.lua
@@ -3,7 +3,7 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = false
 -- Enable the spellcheck functionality
-vim.opt.spell = true
+vim.opt.spell = false
 
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
