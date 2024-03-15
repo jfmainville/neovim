deea13b9399a3bca5c7bf66c5633c51891d54def
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-03-14 20:21:50 -0400

    refactor: remove the actions.save keymap

--- after/plugin/oil.lua
+++ after/plugin/oil.lua
@@ -57,7 +57,6 @@ require("oil").setup({
   -- See :help oil-actions for a list of all available actions
   keymaps = {
     ["g?"] = "actions.show_help",
-    ["za"] = "actions.save",
     ["<CR>"] = "actions.select",
     ["<C-s>"] = "actions.select_vsplit",
     ["<C-h>"] = "actions.select_split",
