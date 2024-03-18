c36b9b22d90e5f962fb0adc4d94171b36b31056c
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-03-17 21:47:48 -0400

    refactor: update the Telescope plugin configuration

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,28 +1,32 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>fs', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
 
-require('telescope').setup({
-  defaults = {
-    -- Remove the ripgrep indentation
-    vimgrep_arguments = {
-      "rg",
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--smart-case",
-      "--trim" -- add this value
-    }
-  },
-  pickers = {
-    find_files = {
-      hidden = false
-    }
-  }
+require("telescope").setup({
+	defaults = {
+		-- Remove the ripgrep indentation
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--smart-case",
+			"--trim", -- add this value
+		},
+	},
+	pickers = {
+		live_grep = {
+			file_ignore_patterns = { "node_modules", ".git", ".venv" },
+			additional_args = function(_)
+				return { "--hidden" }
+			end,
+		},
+		find_files = {
+			hidden = true,
+			file_ignore_patterns = { "node_modules", ".git", ".venv" },
+		},
+	},
 })
