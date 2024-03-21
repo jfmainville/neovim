626b44baebfffbb704fba66b876f6cd8c4cf38aa
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-03-20 21:36:23 -0400

    refactor: update the keymap to show the  current file commit history

--- lua/jfmainville/keymap.lua
+++ lua/jfmainville/keymap.lua
@@ -5,12 +5,12 @@ vim.keymap.set("t", "<S-Space>", "<Space>")
 vim.keymap.set("t", "<S-BS>", "<BS>")
 -- Save the current file in the buffer
 vim.keymap.set("n", "za", function()
-  vim.cmd("w")
+	vim.cmd("w")
 end)
 -- Save file and return to the project explorer view
 vim.keymap.set("n", "zz", function()
-  vim.cmd("w")
-  vim.cmd("Oil")
+	vim.cmd("w")
+	vim.cmd("Oil")
 end)
 -- Move the currently selected line down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -32,18 +32,18 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 -- Source the current file
 vim.keymap.set("n", "<leader><leader>", function()
-  vim.cmd("so")
+	vim.cmd("so")
 end)
 -- Open ChatGPT window
 vim.keymap.set("n", "<leader>cg", "<cmd>ChatGPT<CR>")
 -- Format the file using the Conform plugin
 vim.keymap.set("", "<leader>f", function()
-  require("conform").format({ async = true, lsp_fallback = true })
+	require("conform").format({ async = true, lsp_fallback = true })
 end)
 -- Open the LazyGit window
 vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
 -- Open the LazyGit current file commit history
-vim.keymap.set("n", "<leader>gf", "<cmd>LazyGitFilterCurrentFile<CR>")
+vim.keymap.set("n", "<leader>ggf", "<cmd>LazyGitFilterCurrentFile<CR>")
 -- Start the DAP debugger
 vim.keymap.set("n", "<leader>dc", "<cmd>lua require'dap'.continue()<CR>")
 -- Add a breakpoint DAP debugger
