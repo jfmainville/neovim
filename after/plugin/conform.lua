8848d6c4889c948865273e8217426b48f670d500
Jean-Frederic Mainville <jfm@pixmob.com>
2024-04-05 08:27:28 -0400

    refactor: add the markdown formatter

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,19 +1,20 @@
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		python = { "black" },
-		javascript = { "prettier" },
-		bash = { "shellcheck" },
-		terraform = { "terraform_fmt" },
-		tf = { "terraform_fmt" },
-		["terraform-vars"] = { "terraform_fmt" },
-	},
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
-	cwd = require("conform.util").root_file({ ".editorconfig", "package.json" }),
-	-- When cwd is not found, don't run the formatter (default false)
-	require_cwd = true,
+  formatters_by_ft = {
+    lua = { "stylua" },
+    python = { "black" },
+    javascript = { "prettier" },
+    bash = { "shellcheck" },
+    terraform = { "terraform_fmt" },
+    tf = { "terraform_fmt" },
+    ["terraform-vars"] = { "terraform_fmt" },
+    markdown = { "prettier" },
+  },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+  cwd = require("conform.util").root_file({ ".editorconfig", "package.json" }),
+  -- When cwd is not found, don't run the formatter (default false)
+  require_cwd = true,
 })
