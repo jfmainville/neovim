60bafc1e308194eca111d0d06168ddc5cbddffd5
Jean-Frederic Mainville <jfm@pixmob.com>
2024-04-19 13:28:51 -0400

    refactor: add the git diff indicator to the lualine_b section

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,4 +1,4 @@
-local clients_lsp = function ()
+local clients_lsp = function()
   local bufnr = vim.api.nvim_get_current_buf()
 
   local clients = vim.lsp.buf_get_clients(bufnr)
@@ -14,39 +14,39 @@ local clients_lsp = function ()
 end
 
 require("lualine").setup({
-	options = { theme = "powerline" },
-	extensions = { "oil", "lazy", "mason", "trouble" },
-	sections = {
-		lualine_a = { "mode" },
-		lualine_b = { "branch" },
-		lualine_c = {
-			{
-				"filename",
-				file_status = true, -- Displays file status (readonly status, modified status)
-				newfile_status = true, -- Display new file status (new file means no write after created)
-				path = 3, -- 0: Just the filename
-				-- 1: Relative path
-				-- 2: Absolute path
-				-- 3: Absolute path, with tilde as the home directory
-				-- 4: Filename and parent dir, with tilde as the home directory
+  options = { theme = "powerline" },
+  extensions = { "oil", "lazy", "mason", "trouble" },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff" },
+    lualine_c = {
+      {
+        "filename",
+        file_status = true, -- Displays file status (readonly status, modified status)
+        newfile_status = true, -- Display new file status (new file means no write after created)
+        path = 3,          -- 0: Just the filename
+        -- 1: Relative path
+        -- 2: Absolute path
+        -- 3: Absolute path, with tilde as the home directory
+        -- 4: Filename and parent dir, with tilde as the home directory
 
-				shorting_target = 40, -- Shortens path to leave 40 spaces in the window
-				-- for other components. (terrible name, any suggestions?)
-				symbols = {
-					modified = "●", -- Text to show when the file is modified.
-					readonly = "[Read-Only]", -- Text to show when the file is non-modifiable or readonly.
-					unnamed = "[No Name]", -- Text to show for unnamed buffers.
-					newfile = "[New]", -- Text to show for newly created file before first write
-				},
-			},
-		},
-		lualine_x = {
-			{ "diagnostics", sources = { "nvim_diagnostic", "coc", "nvim_lsp", "vim_lsp" } },
+        shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+        -- for other components. (terrible name, any suggestions?)
+        symbols = {
+          modified = "●", -- Text to show when the file is modified.
+          readonly = "[Read-Only]", -- Text to show when the file is non-modifiable or readonly.
+          unnamed = "[No Name]", -- Text to show for unnamed buffers.
+          newfile = "[New]", -- Text to show for newly created file before first write
+        },
+      },
+    },
+    lualine_x = {
+      { "diagnostics", sources = { "nvim_diagnostic", "coc", "nvim_lsp", "vim_lsp" } },
       clients_lsp,
-			"encoding",
-			"fileformat",
-			"filetype",
-		},
-		lualine_y = { "progress", "searchcount" },
-	},
+      "encoding",
+      "fileformat",
+      "filetype",
+    },
+    lualine_y = { "progress", "searchcount" },
+  },
 })
