efdab7f131a82c9b1f33a4afa210bea95c3b1d93
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-04-29 20:49:29 -0400

    chore: move the CMP plugin configuration to the cmp.lua file

---
+++ after/plugin/cmp.lua
@@ -0,0 +1,35 @@
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local lspkind = require("lspkind")
+
+cmp.setup({
+	sources = {
+		{ name = "path" },
+		{ name = "nvim_lsp" },
+		{ name = "nvim_lua" },
+		{ name = "luasnip", keyword_length = 2 },
+		{ name = "buffer", keyword_length = 3 },
+	},
+	formatting = {
+    format = lspkind.cmp_format({
+      mode = "symbol_text",
+      preset = "codicons",
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                     -- can also be a function to dynamically calculate max width such as 
+                     -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+      ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (entry, vim_item)
+        return vim_item
+      end
+    })
+  },
+	mapping = cmp.mapping.preset.insert({
+		["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
+		["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
+})
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -62,22 +62,3 @@ require("mason-lspconfig").setup({
 		end,
 	},
 })
-
-local cmp = require("cmp")
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-cmp.setup({
-	sources = {
-		{ name = "path" },
-		{ name = "nvim_lsp" },
-		{ name = "nvim_lua" },
-		{ name = "luasnip", keyword_length = 2 },
-		{ name = "buffer", keyword_length = 3 },
-	},
-	formatting = lsp_zero.cmp_format(),
-	mapping = cmp.mapping.preset.insert({
-		["<Tab>"] = cmp.mapping.select_next_item(cmp_select),
-		["<S-Tab>"] = cmp.mapping.select_prev_item(cmp_select),
-		["<CR>"] = cmp.mapping.confirm({ select = true }),
-	}),
-})
