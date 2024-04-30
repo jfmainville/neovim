b6109a7ff2507fe41ac447bddc90bbe2debdf4eb
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-04-29 22:24:44 -0400

    refactor: add additional integrations to the catppuccin colorscheme

--- after/plugin/catppuccin.lua
+++ after/plugin/catppuccin.lua
@@ -37,11 +37,28 @@ require("catppuccin").setup({
 		gitsigns = true,
 		nvimtree = true,
 		treesitter = true,
-		notify = false,
 		mini = {
 			enabled = true,
 			indentscope_color = "",
 		},
-		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = { "italic" },
+				hints = { "italic" },
+				warnings = { "italic" },
+				information = { "italic" },
+			},
+			underlines = {
+				errors = { "underline" },
+				hints = { "underline" },
+				warnings = { "underline" },
+				information = { "underline" },
+			},
+			inlay_hints = {
+				background = true,
+			},
+		},
+    lsp_trouble = true,
 	},
 })
