2c811f58be6a0e0e9c75c54401cda14c7e34fa9e
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-04-29 22:26:09 -0400

    feat: add the hlsearch autocommand base configuration

--- lua/jfmainville/set.lua
+++ lua/jfmainville/set.lua
@@ -34,3 +34,9 @@ vim.opt.updatetime = 50
 
 -- Copy the buffer clipboard to the system clipboard
 vim.api.nvim_set_option("clipboard", "unnamed")
+
+vim.on_key(function(char)
+	if vim.fn.mode() == "n" then
+		vim.opt.hlsearch = vim.tbl_contains({ "<CR>", "n", "N", "*", "#", "?", "/" }, vim.fn.keytrans(char))
+	end
+end, vim.api.nvim_create_namespace("auto_hlsearch"))
