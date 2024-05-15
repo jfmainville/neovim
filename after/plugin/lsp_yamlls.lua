457409771443bc6fbc97cf75fe54ed51a5e02a4e
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-05-14 21:13:41 -0400

    refactor: add the lspconfig local variable

--- after/plugin/lsp_yamlls.lua
+++ after/plugin/lsp_yamlls.lua
@@ -1,23 +1,24 @@
-require('lspconfig').yamlls.setup {
-  settings = {
-    yaml = {
-      completion = true,
-      validate = true,
-      -- disable the schema store
-      schemaStore = {
-        enable = false,
-        url = "",
-      },
-      -- manually select schemas
-      schemas = {
-        ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-        ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-        ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
-        ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
-        ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
-        ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-      }
-    }
-  }
-}
+local lspconfig = require("lspconfig")
+lspconfig.yamlls.setup({
+	settings = {
+		yaml = {
+			completion = true,
+			validate = true,
+			-- disable the schema store
+			schemaStore = {
+				enable = false,
+				url = "",
+			},
+			-- manually select schemas
+			schemas = {
+				["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+				["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+				["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
+				["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+				["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "*gitlab-ci*.{yml,yaml}",
+				["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+				["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+			},
+		},
+	},
+})
