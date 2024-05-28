9bb7b4bb706d5bc7cfe1016f2a67b56c216b2e9d
Jean-Frederic Mainville <jfm@pixmob.com>
2024-05-27 22:13:06 -0400

    refactor: add the settings configuration to the ansible LSP

--- after/plugin/lsp_ansiblels.lua
+++ after/plugin/lsp_ansiblels.lua
@@ -2,4 +2,24 @@ local lspconfig = require("lspconfig")
 lspconfig["ansiblels"].setup({
 	filetypes = { "yaml", "yml", "ansible" },
 	root_dir = lspconfig.util.root_pattern("roles", "playbooks"),
+	settings = {
+		ansible = {
+			ansible = {
+				path = "ansible",
+			},
+			executionEnvironment = {
+				enabled = false,
+			},
+			python = {
+				interpreterPath = "python",
+			},
+			validation = {
+				enabled = true,
+				lint = {
+					enabled = true,
+					path = "ansible-lint",
+				},
+			},
+		},
+	},
 })
