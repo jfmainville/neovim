f018a1e8de036bc9e2cb7dab08e57f9f69170c04
Jean-Frederic Mainville <jfm@pixmob.com>
2024-06-18 13:17:51 -0400

    refactor: update the lualine plugin separators configuration

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -2,8 +2,8 @@ require("lualine").setup({
 	icons_enabled = true,
 	options = {
 		theme = "auto",
-		component_separators = "",
-		section_separators = { left = "", right = "" },
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
 	},
 	disabled_filetypes = {
 		statusline = {},
