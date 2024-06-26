556a7bf6a9325698ed28a34203b500106a6de091
Jean-Frederic Mainville <jfmainville@outlook.com>
2024-06-25 21:05:55 -0400

    refactor: set the vim.opt.relativenumber attribute to true

--- lua/jfmainville/set.lua
+++ lua/jfmainville/set.lua
@@ -2,7 +2,7 @@ vim.opt.guicursor =
 	"n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 vim.opt.nu = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 -- Remove the duplicate mode indicator
 vim.opt.showmode = false
 -- Enable the spellcheck functionality
