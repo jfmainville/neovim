53419b69d3a8363270aac8aa80f7250330681efa
Jean-Frederic Mainville <jfm@pixmob.com>
2024-06-30 22:50:43 -0400

    refactor: add the fzf Telescope plugin

--- lua/jfmainville/packer.lua
+++ lua/jfmainville/packer.lua
@@ -15,6 +15,7 @@ return require("packer").startup(function(use)
 		"nvim-telescope/telescope.nvim",
 		requires = {
 			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
 		},
 	})
 	use("kylechui/nvim-surround")
